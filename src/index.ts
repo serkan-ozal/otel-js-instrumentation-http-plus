65cb59d645c0c83277978246a8eda158d32a8567
Serkan ÖZAL <serkanozal86@gmail.com>
2023-11-28 20:22:23 +0300

    Support capturing HTTP response body with max size limit (#2)

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,19 @@ import {
 } from '@opentelemetry/instrumentation-http';
 
 const DEFAULT_MAX_REQUEST_BODY_SIZE: number = 4 * 1024; // 4 KB
+const DEFAULT_MAX_RESPONSE_BODY_SIZE: number = 4 * 1024; // 4 KB
 
 export const HttpPlusSemanticAttributes = {
     HTTP_REQUEST_BODY: 'http.request.body',
+    HTTP_RESPONSE_BODY: 'http.response.body',
 };
 
 export interface HttpPlusInstrumentationConfig
     extends HttpInstrumentationConfig {
     captureRequestBody?: boolean;
     maxRequestBodySize?: number;
+    captureResponseBody?: boolean;
+    maxResponseBodySize?: number;
 }
 
 export class HttpPlusInstrumentation extends HttpInstrumentation {
@@ -44,14 +48,16 @@ export class HttpPlusInstrumentation extends HttpInstrumentation {
     ): HttpRequestCustomAttributeFunction {
         return (span: Span, request: ClientRequest | IncomingMessage): void => {
             const instrumentation = this;
+
             if (request instanceof ClientRequest) {
                 const originalWrite = request.write.bind(request);
                 const originalEnd = request.end.bind(request);
 
-                const maxRequestBodySize: number =
-                    config?.maxRequestBodySize || DEFAULT_MAX_REQUEST_BODY_SIZE;
-
                 if (config && config.captureRequestBody) {
+                    const maxRequestBodySize: number =
+                        config?.maxRequestBodySize ||
+                        DEFAULT_MAX_REQUEST_BODY_SIZE;
+
                     request.write = (data: any): boolean => {
                         if (
                             typeof data === 'string' ||
@@ -79,7 +85,6 @@ export class HttpPlusInstrumentation extends HttpInstrumentation {
                         }
                         return originalWrite(data);
                     };
-
                     request.end = (data: any): ClientRequest => {
                         if (
                             typeof data === 'string' ||
@@ -123,6 +128,54 @@ export class HttpPlusInstrumentation extends HttpInstrumentation {
             span: Span,
             response: IncomingMessage | ServerResponse
         ): void => {
+            const instrumentation = this;
+
+            if (config && config.captureResponseBody) {
+                const maxResponseBodySize: number =
+                    config?.maxResponseBodySize ||
+                    DEFAULT_MAX_RESPONSE_BODY_SIZE;
+
+                let chunks: Buffer[] | null = [];
+                let totalSize: number = 0;
+                response.prependListener('data', (chunk: any): void => {
+                    if (!chunk) {
+                        return;
+                    }
+                    if (typeof chunk === 'string' || chunk instanceof Buffer) {
+                        totalSize += chunk.length;
+                        if (chunks && totalSize <= maxResponseBodySize) {
+                            chunks.push(
+                                typeof chunk === 'string'
+                                    ? Buffer.from(chunk)
+                                    : chunk
+                            );
+                        } else {
+                            // No need to capture partial response body
+                            chunks = null;
+                        }
+                    }
+                });
+                response.prependOnceListener('end', (): void => {
+                    try {
+                        if (chunks && chunks.length) {
+                            const concatedChunks: Buffer =
+                                Buffer.concat(chunks);
+                            const responseBody: string =
+                                concatedChunks.toString('utf8');
+                            span.setAttribute(
+                                HttpPlusSemanticAttributes.HTTP_RESPONSE_BODY,
+                                responseBody
+                            );
+                        }
+                    } catch (e) {
+                        instrumentation._diag.error(
+                            'Error occurred while capturing response body:',
+                            e
+                        );
+                    }
+                });
+            }
+
             if (originalResponseHook) {
                 originalResponseHook(span, response);
             }
